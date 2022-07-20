c4da95e928a50e07d32aba537eb68623b5a857f1
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-07-20 15:17:13 -0700

    add JavaScript for basic name tag functionality

--- app.js
+++ app.js
@@ -6,3 +6,12 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const userNameInput = document.getElementById('text');
+const submit = document.getElementById('submit');
+const userName = document.getElementById('name');
+
+submit.addEventListener('click', () => {
+    const value = userNameInput.value;
+    userName.textContent = value;
+    console.log(value);
+});
