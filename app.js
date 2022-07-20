17d59b4935cd9037bf44fdfc0c7b591be1a28f28
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-07-20 15:52:47 -0700

    add pronouns input and display

--- app.js
+++ app.js
@@ -6,12 +6,18 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
-const userNameInput = document.getElementById('text');
+const userNameInput = document.getElementById('userName');
+const userPronounInput = document.getElementById('userPronoun');
 const submit = document.getElementById('submit');
 const userName = document.getElementById('name');
+const userPronouns = document.getElementById('pronouns');
+
 
 submit.addEventListener('click', () => {
     const value = userNameInput.value;
     userName.textContent = value;
-    console.log(value);
+});
+submit.addEventListener('click', () =>{
+    const valueTwo = userPronounInput.value;
+    userPronouns.textContent = valueTwo;
 });
